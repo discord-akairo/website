374642a21675dcd263eab76e7d227276f8b14ab0
1computer1 <mineox100@gmail.com>
2019-03-23 23:27:46 -0400

    Change tag filters to include future versions only

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -1,7 +1,7 @@
 import semver from 'semver';
 import DocsSource from './DocsSource';
 
-const branchBlacklist = new Set(['docs', 'gh-pages', '7.x']);
+const branchBlacklist = new Set(['docs', 'gh-pages', 'parsers', 'typehandler']);
 export default new DocsSource({
   id: 'main',
   name: 'Main library',
@@ -10,5 +10,5 @@ export default new DocsSource({
   defaultTag: 'master',
   defaultClass: 'AkairoClient',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
-  tagFilter: tag => semver.gte(tag, '8.0.0'),
+  tagFilter: tag => (semver.gte(tag, '7.5.6') && semver.lt(tag, '8.0.0-beta.1')) || semver.gt(tag, '8.0.0-beta.3'),
 });
