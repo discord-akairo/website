7a7d7742d2a1d6542e114cd9688e1c57130b454f
1computer1 <22125769+1Computer1@users.noreply.github.com>
2020-03-03 11:37:36 -0500

    default to master branch

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -7,7 +7,7 @@ export default new DocsSource({
   name: 'Main library',
   global: 'Akairo',
   repo: 'discord-akairo/discord-akairo',
-  defaultTag: 'stable',
+  defaultTag: 'master',
   defaultClass: 'AkairoClient',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
   tagFilter: tag => (semver.gte(tag, '7.5.6') && semver.lt(tag, '8.0.0-beta.1')) || semver.gt(tag, '8.0.0-beta.3'),
